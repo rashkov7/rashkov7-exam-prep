e13e59d67f5f9cec4b2e19d6449dfa5f68e7b237
Georgi Rashkov <georgi.rashkov@dxc.com>
2024-06-24 23:21:04 +0300

    fix some tests 3

--- tests/ui/header.test.js
+++ tests/ui/header.test.js
@@ -4,6 +4,6 @@ test('Check header', async ({ page }) => {
     await page.goto('http://localhost:8080'); 
     const homeLink = await page.$('h1');
     const text = await homeLink.textContent();
-    expect(text).toBe('My Shopping List');
+    expect(text).toBe('My Shopping list');
   });
   
